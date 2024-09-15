41255feb1ab61de277824e42bde32be8c65e08ef
Alexandre Picavet <apicavet@hotmail.fr>
2024-09-15 19:14:07 +0200

    feat(zsh): update fzf tab completion and tmux popup size

--- .config/zsh/plugins/configurations/fzf-tab.zsh
+++ .config/zsh/plugins/configurations/fzf-tab.zsh
@@ -7,11 +7,13 @@ if check_install fzf; then
 	zstyle ':fzf-tab:*' switch-group '<' '>'
 	if check_install tmux; then
 		zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+		zstyle ':fzf-tab:*' popup-min-size 100 10
 	fi
 
 	zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
 	zstyle ':fzf-tab:complete:eza:*' fzf-preview 'eza -1 --color=always $realpath'
-	zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status $word'
+	zstyle ':fzf-tab:complete:rm:*' fzf-preview 'eza -1 --color=always $realpath'
+	zstyle ':fzf-tab:complete:systemctl-*:*' fzf-preview 'SYSTEMD_COLORS=1 systemctl status -- $word'
 	zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
 	zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-preview '[[ $group == "[process ID]" ]] && ps --pid=$word -o cmd --no-headers -w -w'
 	zstyle ':fzf-tab:complete:(kill|ps):argument-rest' fzf-flags --preview-window=down:3:wrap
