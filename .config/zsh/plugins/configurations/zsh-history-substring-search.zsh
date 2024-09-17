1c3891490f6913be9f1a14696ac4d5fffb8498dc
Alexandre Picavet <apicavet@hotmail.fr>
2024-09-17 16:17:50 +0200

    feat(zsh): Update zsh history substring search key bindings

--- .config/zsh/plugins/configurations/zsh-history-substring-search.zsh
+++ .config/zsh/plugins/configurations/zsh-history-substring-search.zsh
@@ -4,7 +4,9 @@ HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND="bg=green,fg=black,bold"
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND="bg=red,fg=black,bold"
 
 bindkey '^[[A' history-substring-search-up
+bindkey '\eOA' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
+bindkey '\eOB' history-substring-search-down
 
 bindkey -M vicmd 'k' history-substring-search-up
 bindkey -M vicmd 'j' history-substring-search-down
