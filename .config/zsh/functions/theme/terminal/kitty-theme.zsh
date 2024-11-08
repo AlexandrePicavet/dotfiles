3437d5abb0791f65c6da057bc99ae4da23d8d58a
Alexandre Picavet <apicavet@hotmail.fr>
2024-11-08 16:46:17 +0100

    feat(kitty): Update theme-switch to work offline

--- .config/zsh/functions/theme/terminal/kitty-theme.zsh
+++ .config/zsh/functions/theme/terminal/kitty-theme.zsh
@@ -1,23 +1,16 @@
 function kitty-theme() {
 	which kitty &>/dev/null || return 0
 
-	local -r LIGHT_THEME_NAME='Catppuccin-Latte'
-	local -r DARK_THEME_NAME='Catppuccin-Macchiato'
+	local -r CONFIG_FILE=~/.config/kitty/kitty.conf
+	local -r LIGHT_THEME_MATCH='include themes\/catppuccin-latte.conf'
+	local -r DARK_THEME_MATCH='include themes\/catppuccin-macchiato.conf'
 
 	local THEME
 	THEME="${1}"
 	readonly THEME
 
-	local THEME_NAME
-	if [[ "${THEME}" == 'light' ]]; then
-		THEME_NAME="${LIGHT_THEME_NAME}"
-	elif [[ "${THEME}" == 'dark' ]]; then
-		THEME_NAME="${DARK_THEME_NAME}"
-	else
-		echo "Expecting first argument to be 'light' or 'dark' but got: '${THEME}'" >&2
-		return 1
-	fi
-	readonly THEME_NAME
+	_theme_helper "${CONFIG_FILE}" "${LIGHT_THEME_MATCH}" "${DARK_THEME_MATCH}" "${THEME}"
 
-	kitty +kitten themes --reload-in=all "${THEME_NAME}"
+	# Reload config
+	kill -SIGUSR1 $(pgrep kitty)
 }
