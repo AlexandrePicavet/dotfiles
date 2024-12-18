c45f01562022d30c8dbd819afb4b9d3a3a976206
Alexandre Picavet <apicavet@hotmail.fr>
2024-12-18 19:00:32 +0100

    feat(neovim): Update lualine config

--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -9,5 +9,16 @@ return {
 			component_separators = "",
 			section_separators = { left = "", right = "" },
 		},
+		sections = {
+			lualine_a = {
+				{
+					"mode",
+					fmt = function(str)
+						return str:sub(1, 1)
+					end,
+				},
+			},
+			lualine_x = {}
+		},
 	},
 }
