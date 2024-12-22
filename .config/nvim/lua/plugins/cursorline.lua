d16b814862b385c6349d10936159d61ff149597f
Alexandre Picavet <apicavet@hotmail.fr>
2024-12-22 22:09:50 +0100

    feat(neovim): Add cursorline

---
+++ .config/nvim/lua/plugins/cursorline.lua
@@ -0,0 +1,17 @@
+return {
+	"ya2s/nvim-cursorline",
+	opts = {
+		cursorline = {
+			enable = true,
+			timeout = 0,
+			number = true,
+		},
+		cursorword = {
+			enable = false,
+			hl = {
+				underline = false,
+			},
+		},
+	},
+	config = true,
+}
