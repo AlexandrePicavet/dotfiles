413af7192b4025aad8a8241f78b0b9a78ff8fe8b
Alexandre Picavet <apicavet@hotmail.fr>
2025-01-16 16:47:17 +0100

    feat(eww): Add hyprland window title & some fixes

---
+++ .config/eww/scripts/hyprland/get-window-title.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# https://wiki.hyprland.org/Useful-Utilities/Status-Bars/#configewwscriptsget-window-title
+
+hyprctl activewindow -j | jq --raw-output .title
+
+socat -u "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" - | stdbuf -o0 awk -F '>>|,' '/^activewindow>>/{print $3}'
