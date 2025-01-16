76971b0885b73f154a38fef2f580c2007a5ac620
Alexandre Picavet <apicavet@hotmail.fr>
2025-01-16 15:13:39 +0100

    feat(eww): Add hyprland workspaces widget

---
+++ .config/eww/scripts/hyprland/get-active-workspace.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# https://wiki.hyprland.org/Useful-Utilities/Status-Bars/#configewwscriptsget-active-workspace
+
+hyprctl monitors -j | jq '.[] | select(.focused) | .activeWorkspace.id'
+
+socat -u "UNIX-CONNECT:${XDG_RUNTIME_DIR}/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock" - |
+	stdbuf -o0 awk -F '>>|,' -e '/^workspace>>/ {print $2}' -e '/^focusedmon>>/ {print $3}'
