f466852c810b8d85d8f23f511dc230b293f39181
Alexandre Picavet <apicavet@hotmail.fr>
2025-02-07 00:28:52 +0100

    feat(eww): Update topbar script & kill both script & eww daemon upon hyprland exit

--- .config/eww/topbar.sh
+++ .config/eww/topbar.sh
@@ -2,17 +2,21 @@
 
 set -euo pipefail
 
-MONITOR_PREFIX="monitor:"
+declare -r LOG_FILE="/tmp/eww-topbar.log"
+
+function log() {
+	echo ${*} | tee -a "${LOG_FILE}"
+}
 
 function open-topbar() {
 	local -r monitor="${1}"
 	local -r id="topbar-${monitor}"
 
-	echo "Opening topbar with id '${id}' on monitor '${monitor}'..."
+	log "Opening topbar with id '${id}' on monitor '${monitor}'..."
 	if eww open topbar --id "${id}" --arg "monitor=${monitor}" 1>/dev/null; then
-		echo -e '\t\e[32mOK\e[0m'
+		log -e '\t\e[32mOK\e[0m'
 	else
-		echo -e '\t\e[31mFailed\e[0m'
+		log -e '\t\e[31mFailed\e[0m'
 	fi
 }
 
@@ -29,13 +33,12 @@ function open-on-all-monitors() {
 function listen-monitor-updates() {
 	socat -u "UNIX-CONNECT:${XDG_RUNTIME_DIR}/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock" - |
 		stdbuf -o0 awk -F '>>|,' -e '/^monitoradded>>/{print $2}' |
-		xargs -I % "$0" "${MONITOR_PREFIX}%"
+		while read -r monitor; do
+			log "monitor: ${monitor}"
+			open-topbar "${monitor}"
+		done
 }
 
-if [ -z "${1:-}" ]; then
-	open-on-all-monitors
-	listen-monitor-updates
-elif [[ ${1} =~ ^${MONITOR_PREFIX} ]]; then
-	sleep 1
-	open-topbar "${1##"${MONITOR_PREFIX}"}"
-fi
+:> ${LOG_FILE}
+open-on-all-monitors
+listen-monitor-updates
