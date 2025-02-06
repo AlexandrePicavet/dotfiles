81907f5a9e7c23085c800aedec94af485fc507af
Alexandre Picavet <apicavet@hotmail.fr>
2025-02-06 18:05:17 +0100

    feat(eww): Fix topbar script

--- .config/eww/topbar.sh
+++ .config/eww/topbar.sh
@@ -2,19 +2,40 @@
 
 set -euo pipefail
 
-function open-topbar() {
-	declare -r monitor="${1}"
-	declare -r id="topbar-${monitor}"
+MONITOR_PREFIX="monitor:"
 
-	eww open topbar --id "${id}" --arg "monitor=${monitor}" || true
+function open-topbar() {
+	local -r monitor="${1}"
+	local -r id="topbar-${monitor}"
+
+	echo "Opening topbar with id '${id}' on monitor '${monitor}'..."
+	if eww open topbar --id "${id}" --arg "monitor=${monitor}" 1>/dev/null; then
+		echo -e '\t\e[32mOK\e[0m'
+	else
+		echo -e '\t\e[31mFailed\e[0m'
+	fi
 }
 
-while read -r monitor; do
-	open-topbar "${monitor}"
-done <<< "$(hyprctl monitors -j | jq -r '.[].name')"
+function list-monitors() {
+	hyprctl monitors -j | jq -r '.[].name'
+}
 
-socat -u "UNIX-CONNECT:${XDG_RUNTIME_DIR}/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock" - |
-	awk -F '>>|,' -e '/^monitoradded>>/{print $2}' |
+function open-on-all-monitors() {
 	while read -r monitor; do
 		open-topbar "${monitor}"
-	done
+	done <<<"$(list-monitors)"
+}
+
+function listen-monitor-updates() {
+	socat -u "UNIX-CONNECT:${XDG_RUNTIME_DIR}/hypr/${HYPRLAND_INSTANCE_SIGNATURE}/.socket2.sock" - |
+		stdbuf -o0 awk -F '>>|,' -e '/^monitoradded>>/{print $2}' |
+		xargs -I % "$0" "${MONITOR_PREFIX}%"
+}
+
+if [ -z "${1:-}" ]; then
+	open-on-all-monitors
+	listen-monitor-updates
+elif [[ ${1} =~ ^${MONITOR_PREFIX} ]]; then
+	sleep 1
+	open-topbar "${1##"${MONITOR_PREFIX}"}"
+fi
