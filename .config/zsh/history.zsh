1d81da614b89b8cad917fbb43622e282500a7963
Alexandre Picavet <apicavet@hotmail.fr>
2025-05-22 09:20:16 +0200

    feat(zsh): Disable history options

--- .config/zsh/history.zsh
+++ .config/zsh/history.zsh
@@ -6,16 +6,16 @@ HISTSIZE=9223372036854775807
 SAVEHIST=9223372036854775807
 
 # Includes timestamp
-setopt EXTENDED_HISTORY
+# setopt EXTENDED_HISTORY
 # Beeps if attempting to access a history entry which isn't there
-setopt HIST_BEEP
+# setopt HIST_BEEP
 # Does not save if command is a duplicate of the previous one
-setopt HIST_IGNORE_DUPS
+# setopt HIST_IGNORE_DUPS
 # Does not save if line starts with space
-setopt HIST_IGNORE_SPACE
+# setopt HIST_IGNORE_SPACE
 # Strips superfluous blanks
-setopt HIST_REDUCE_BLANKS
+# setopt HIST_REDUCE_BLANKS
 # Expands the line without executing it
-setopt HIST_VERIFY
+# setopt HIST_VERIFY
 # Imports and appends new commands from and to the history
-setopt SHARE_HISTORY
+# setopt SHARE_HISTORY
