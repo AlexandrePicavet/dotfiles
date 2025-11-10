ac42526bd756434de8a8d94f092a642a6db52220
Alexandre Picavet <apicavet@hotmail.fr>
2025-11-10 09:11:22 +0100

    feat(nvim): Use eslint format for typescript files

---
+++ .config/nvim/after/lsp/ts_ls.lua
@@ -0,0 +1,22 @@
+local function eslint_format()
+    vim.lsp.buf.format({
+        filter = function(client)
+            return client.name == "eslint"
+        end,
+    })
+end
+
+-- Override default formatter
+vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function()
+        -- TODO check if eslint is configured before overriding keymap
+        vim.keymap.set(
+            { "i", "n", "v" },
+            "<C-f>",
+            eslint_format,
+            { desc = "Format Typescript file with ESLint", noremap = true }
+        )
+    end,
+})
+
+return {}
