eea889c8fe46237906df3e790a0f9e1c44827cda
Alexandre Picavet <apicavet@hotmail.fr>
2025-11-14 20:57:54 +0100

    perf(wallpaper-update): Fix dumb wallpaper update infinite loop draining battery

--- .config/hypr/scripts/update-current-wallpaper.sh
+++ .config/hypr/scripts/update-current-wallpaper.sh
@@ -1,33 +1,41 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
 readonly BASEDIR="${HOME}/.config/hypr/user/wallpapers"
 readonly CURRENT="${BASEDIR}/current"
+
+declare wallpaper_count
+wallpaper_count="$(find "${BASEDIR}" -maxdepth 1 -type f -not -name '.*' | wc -l)"
+readonly wallpaper_count
+
+if [ "${wallpaper_count}" -eq 0 ]; then
+	sleep 5
+	notify-send 'Missing wallpapers' "You can put wallpapers into the directory: ${BASEDIR}"
+	return 1
+fi
 
 declare current_wallpaper
 current_wallpaper="$(readlink "${CURRENT}" || echo "")"
 readonly current_wallpaper
 
 declare new_wallpaper
-while
-	new_wallpaper="$(find "${BASEDIR}" -maxdepth 1 -type f -not -name '.*' | sort --random-sort | head -n1)"
-	[ "${new_wallpaper}" == "${current_wallpaper}" ]
-do true; done
+new_wallpaper="$(find \
+	"${BASEDIR}" \
+	-maxdepth 1 \
+	-type f \
+	-not -name '.*' \
+	-not -name "$(basename "${current_wallpaper}")" \
+		| sort --random-sort \
+		| head -n1 \
+)"
 readonly new_wallpaper
 
 
-if [ -z "${new_wallpaper}" ]; then
-	sleep 5
-	notify-send 'Missing wallpapers' "You can put wallpapers into the directory: ${BASEDIR}"
-else
-	echo "Changing wallpaper to: $(basename "${new_wallpaper}")."
+echo "Changing wallpaper to: $(basename "${new_wallpaper}")."
 
-	ln -fs "${new_wallpaper}" "${CURRENT}"
+ln -fs "${new_wallpaper}" "${CURRENT}"
 
-	hyprctl hyprpaper preload "${new_wallpaper}" &>/dev/null
-	hyprctl hyprpaper wallpaper ",${new_wallpaper}" &>/dev/null
-	hyprctl hyprpaper unload all &>/dev/null
-fi
-
-
+hyprctl hyprpaper preload "${new_wallpaper}" &>/dev/null
+hyprctl hyprpaper wallpaper ",${new_wallpaper}" &>/dev/null
+hyprctl hyprpaper unload all &>/dev/null
