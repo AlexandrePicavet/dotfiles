56708671cb431ead420d2bb1c9c3f040cd4c406c
Alexandre Picavet <apicavet@hotmail.fr>
2025-11-19 09:40:53 +0100

    fix(nvim): Fix formatting

--- .config/nvim/after/lsp/ts_ls.lua
+++ .config/nvim/after/lsp/ts_ls.lua
@@ -1,22 +1,23 @@
 local function eslint_format()
-    vim.lsp.buf.format({
-        filter = function(client)
-            return client.name == "eslint"
-        end,
-    })
+	vim.lsp.buf.format({
+		filter = function(client)
+			return client.name == "eslint"
+		end,
+	})
 end
 
 -- Override default formatter
-vim.api.nvim_create_autocmd("BufEnter", {
-    callback = function()
-        -- TODO check if eslint is configured before overriding keymap
-        vim.keymap.set(
-            { "i", "n", "v" },
-            "<C-f>",
-            eslint_format,
-            { desc = "Format Typescript file with ESLint", noremap = true }
-        )
-    end,
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+	callback = function()
+		-- TODO check if eslint is configured before overriding keymap
+		vim.keymap.set(
+			{ "i", "n", "v" },
+			"<C-f>",
+			eslint_format,
+			{ desc = "Format Typescript file with ESLint", noremap = true }
+		)
+	end,
 })
 
 return {}
