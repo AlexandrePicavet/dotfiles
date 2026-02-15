84e5f80c40ffd4be2ccbe31e6733e4997412ac57
Alexandre Picavet <apicavet@hotmail.fr>
2026-02-16 00:51:44 +0100

    feat(nvim): Fix java snippets

--- .config/nvim/snippets/java.lua
+++ .config/nvim/snippets/java.lua
@@ -1,76 +1,53 @@
 local ls = require("luasnip")
-local snippet = ls.snippet
-local snippet_node = ls.snippet_node
-local function_node = ls.function_node
-local choice_node = ls.choice_node
-local text_node = ls.text_node
-local insert_node = ls.insert_node
+local s = ls.snippet
+local sn = ls.snippet_node
+local f = ls.function_node
+local c = ls.choice_node
+local t = ls.text_node
+local i = ls.insert_node
 
-local function filename_base()
+local function package()
+	return "package "
+		.. vim.fn.expand("%:p:h")
+			:gsub("^.*/main/java/(.+)", "%1")
+			:gsub("^.*/test/java/(.+)", "%1")
+			:gsub("/", ".")
+		.. ";"
+end
+
+local function classname()
 	return vim.fn.expand("%:t:r")
 end
 
 ls.add_snippets("java", {
-	snippet("nf", {
-		function_node(function(_, snip)
-			return "package "
-				.. snip.env.RELATIVE_FILEPATH
-					:gsub("^.*/main/java/(.+)/[a-zA-Z0-9]+.java", "%1")
-					:gsub("^.*/test/java/(.+)/[a-zA-Z0-9]+.java", "%1")
-					:gsub("/", ".")
-				.. ";"
-		end, {}),
-		text_node({ "", "", "" }),
-		text_node("public "),
-		choice_node(1, {
-			text_node("class"),
-			text_node("enum"),
-			text_node("interface"),
-			text_node("abstract class"),
-			text_node("final class"),
+	s("nf", {
+		f(package, {}),
+		t({ "", "", "public " }),
+		c(1, { t("class"), t("enum"), t("interface"), t("abstract class"), t("final class") }),
+		t(" "),
+		f(classname, {}),
+		c(2, {
+			t(""),
+			sn("implements", { t(" implements "), i(1, "Interface") }),
+			sn("extends", { t(" extends "), i(1, "Class") }),
 		}),
-		text_node(" "),
-		function_node(filename_base, {}),
-		choice_node(2, {
-			text_node(""),
-			snippet_node("implements", {
-				text_node(" implements "),
-				insert_node(1),
-			}),
-			snippet_node("extends", {
-				text_node(" extends "),
-				insert_node(1),
-			}),
-		}),
-		text_node({ " {", "" }),
-		text_node("\t"),
-		insert_node(0),
-		text_node({ "", "" }),
-		text_node("}"),
+		t({ " {", "" }),
+		t("\t"),
+		i(0),
+		t({ "", "" }),
+		t("}"),
 	}),
-	snippet("nfr", {
-		function_node(function(_, snip)
-			return "package "
-				.. snip.env.RELATIVE_FILEPATH
-					:gsub("^.*/main/java/(.+)/[a-zA-Z0-9]+.java", "%1")
-					:gsub("^.*/test/java/(.+)/[a-zA-Z0-9]+.java", "%1")
-					:gsub("/", ".")
-				.. ";"
-		end, {}),
-		text_node({ "", "", "" }),
-		text_node("public record "),
-		function_node(filename_base, {}),
-		text_node({ " {", "" }),
-		text_node("\t"),
-		insert_node(0),
-		text_node({ "", ")" }),
-		choice_node(1, {
-			text_node(""),
-			snippet_node("implements", {
-				text_node(" implements "),
-				insert_node(1),
-			}),
+	s("nfr", {
+		f(package, {}),
+		t({ "", "", "public record " }),
+		f(classname, {}),
+		t({ " (", "\t" }),
+		i(0),
+		t({ "", ")" }),
+		c(1, {
+			t(""),
+			sn("implements", { t(" implements "), i(1, "Interface") }),
 		}),
-		text_node(" {}")
+		t(" {}"),
 	}),
 })
