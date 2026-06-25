439f3388f42faccbc8c4531e449889a03c50f149
Alexandre Picavet <contact@picavet.dev>
2026-06-25 16:23:08 +0200

    feat(git): Add mergiraf as a merge driver

--- git/.#setup.sh
+++ git/.#setup.sh
@@ -5,3 +5,18 @@ touch ~/.user.gitconfig || {
 	error 'Failed to create the .user.gitconfig file'
 	return ${code}
 }
+
+mkdir -p ~/.config/git || {
+	declare -ri code=$?
+	error 'Failed to create the .config/git directory'
+	return ${code}
+}
+
+{
+	echo -n >~/.config/git/attributes
+	find "$(git rev-parse --show-toplevel)" -name '*.gitattributes' -exec cat '{}' + >>~/.config/git/attributes
+} || {
+	declare -ri code=$?
+	error 'Failed to merge gitattributes files into the .config/git/attributes'
+	return ${code}
+}
