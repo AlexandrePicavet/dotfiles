83811bf62eb0ad4bb4666b90663ea4281ec63293
Alexandre Picavet <contact@picavet.dev>
2026-07-31 09:46:04 +0200

    feat(zsh): Update gtk theme switching to add understandable error message then failing to switch color scheme

--- gtk/dot-local/share/zsh/theming/gtk.zsh
+++ gtk/dot-local/share/zsh/theming/gtk.zsh
@@ -5,10 +5,14 @@ function _gtk_theme() {
 	THEME="${1}"
 	readonly THEME
 
-	_custom_theme_helper \
+	local result
+	result="$(_custom_theme_helper \
 		"gsettings set org.gnome.desktop.interface color-scheme prefer-light" \
 		"gsettings set org.gnome.desktop.interface color-scheme prefer-dark" \
-		"${THEME}"
+		"${THEME}" 2>&1)"
+
+	[ "${result}" = "No schemas installed" ] &&
+		echo "There was a problem while switching the GTK color scheme. Please open a new terminal and try again" >&2
 }
 
 THEMES+=(_gtk_theme)
